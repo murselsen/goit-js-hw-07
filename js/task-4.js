6c02fb67b20b87632aee41485cd82dfdfc0d4fa7
Mürsel SEN <murselsen_35@hotmail.com>
2025-02-03 14:08:50 +0300

    Add form submission handling with validation in task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,15 @@
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const email = form.elements.email.value.trim();
+  const password = form.elements.password.value.trim();
 
+  if (!email || !password) {
+    alert("All form fields must be filled in");
+    return;
+  }
+
+  console.log("Email: ", email);
+  console.log("Password: ", password);
+  form.reset();
+});
