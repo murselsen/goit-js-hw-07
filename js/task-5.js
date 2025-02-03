531cad9186afa26bb06200ef34f0620f222431c2
Mürsel SEN <murselsen_35@hotmail.com>
2025-02-03 14:08:55 +0300

    Refactor getRandomHexColor to an arrow function and add event listener to change background color

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,10 @@
-function getRandomHexColor() {
+const getRandomHexColor = () => {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-}
+};
+
+document.querySelector(".change-color").addEventListener("click", (e) => {
+  document.body.style.backgroundColor = getRandomHexColor();
+  document.querySelector(".color").innerText = getRandomHexColor();
+});
