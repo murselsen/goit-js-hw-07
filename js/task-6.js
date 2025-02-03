d5dd035ff6c4b04e5eee9ca44f54d4311527c23a
Mürsel SEN <murselsen_35@hotmail.com>
2025-02-03 14:08:58 +0300

    Add form submission to create dynamic boxes with random colors

--- js/task-6.js
+++ js/task-6.js
@@ -1,5 +1,34 @@
-function getRandomHexColor() {
+const form = document.querySelector("#controls");
+const boxes = document.querySelector("#boxes");
+let boxItem = {
+  width: 30,
+  height: 30,
+};
+
+const getRandomHexColor = () => {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-}
+};
+const createBox = (w, h) => {
+  const box = document.createElement("div");
+  box.style.width = w + "px";
+  box.style.height = h + "px";
+  box.style.backgroundColor = getRandomHexColor();
+  return box;
+};
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  boxes.innerHTML = " ";
+  boxItem = {
+    width: 30,
+    height: 30,
+  };
+  const formboxCount = form.elements[0].value;
+  for (let index = 0; index < formboxCount; index++) {
+    boxes.appendChild(createBox(boxItem.width, boxItem.height));
+    boxItem.width += 10;
+    boxItem.height += 10;
+  }
+});
