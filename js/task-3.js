736989523e9e88ce3a5c417e4585007a14024065
Mürsel SEN <murselsen_35@hotmail.com>
2025-02-17 13:04:32 +0300

    feat: update 1 files (js/task-3.js) at 10:04

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,7 @@
 document.querySelector("#name-input").addEventListener("input", (event) => {
   event.preventDefault();
   let result = event.target.value.trim();
+  console.log(result);
   if (result !== null && result !== "" && result !== " ") {
     document.querySelector("#name-output").innerText = result;
   } else {
